25c51aae8e2419abb01d70278adcc566a201554a
Ignacy Klimont <ignacyklimont@gmail.com>
2022-06-03 12:05:00 +0200

    Ok

--- MDA_Test_File.py
+++ MDA_Test_File.py
@@ -1,3 +1,10 @@
 # This is a sample Python script.
 print('Hello')
 print('Uploading')
+import pandas as pd
+
+url = 'https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes/blob/master/all/all.csv'
+df = pd.read_csv(url, )
+# df = pd.read_csv(url)
+
+print(df.head(5))
